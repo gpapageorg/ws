6cb420255e5e17d23e8b8db578352c565da6ff98
george <georg.papageorg@gmail.com>
2023-02-16 10:24:06 +0200

    Initial Commit

--- BluetoothComunication/main.py
+++ BluetoothComunication/main.py
@@ -7,7 +7,7 @@ PORT = '/dev/rfcomm0'
 class Data:
     def __init__(self, string):
         self.__id = string[0]
-        self.__temp = string[1] / 100
+        self.__temp = string[1] / 10
         self.__humidity = string[2] 
         self.__pressure = string[3] if len(string) >= 4 else -300
         self.__alt = string[4] if len(string) >= 5 else -300
@@ -34,19 +34,34 @@ class Device:
         try:
             self.device = serial.Serial(self.port, 9600, timeout = 1)
         except:
-            print("Error")
+            print("Error, Probably Adapter Not Connected")
             exit()
                 
     def make_query(self):
-        self.device.write('fetch'.encode('utf-8'))
+        try:
+            #self.device.write('trash\n'.encode('utf-8'))
+            self.device.write('fetch\n'.encode('utf-8'))
+            print("Debug: Query Made, Awaiting Response")
+        except:
+            print("Error, Can't Make Query")
     
     def get_text(self):
         self.inc = ''
         if(self.device.in_waiting != 0):
             self.inc = str(self.device.readline().decode('utf-8'))
+            print("Debug: Response Arrived")
 
-            return self.inc
-        
+        #if(re.match('^[0-9]\s',self.inc) == None):
+        #    print("None Arrived Trying Again In 3s")
+        #    self.inc = ''
+        #    time.sleep(3)
+        #    self.make_query()
+        #    time.sleep(1)
+        #    self.get_text()
+        #else:
+        #print(len(self.inc))
+        return self.inc
+
     def __del__(self):
         pass
        # self.device.close()
@@ -58,9 +73,13 @@ def split_string(inc):
     data = []
     strings = []
     finValues = []
+    
+    print(inc)
 
-
-    inc = re.sub(" +", " ", inc)
+    try:
+        inc = re.sub(" +", " ", inc)
+    except:
+        print("Error In Splitting")
 
     for x in inc:
         if (x == "|" or x == "\n"):
@@ -72,14 +91,14 @@ def split_string(inc):
         strings.append(data[x].split(' '))
 
     for x in range(len(strings)):
-        strings[x] = [float(y) for y in strings[x]]
-        strings[x][0] = int(strings[x][0])
+        strings[x] = [int(y) for y in strings[x]]
+        #strings[x][0] = int(strings[x][0])
 
     for x in strings:
         finValues.append(Data(x))
     
 
-    return finValues
+    return finValues;
 
 
 def main():
@@ -89,12 +108,31 @@ def main():
     ble.make_query()
     time.sleep(1)
     data = ble.get_text()
-    print(data, end="")
+    t = time.localtime()
+    print(time.strftime("%H:%M:%S", t))
 
-    #receivedData = split_string(data)
+
+    receivedData = split_string(data)
+
+    #print(len(receivedData))
+
+    #printData(receivedData)
     
-   # del ble
-   # exit()
+    del ble
+    #exit()
 
+
+def printData(values):
+    for x in range(len(values)):
+        print("Got From: ", end = '')
+        print(values[x].getId())
+        print(values[x].getTemp())
+        print(values[x].getHumidity())
+        print(values[x].getPressure())
+        print(values[x].getAlt())
+
+     
 # Call Main Function
-main()
+while(True):
+    main()
+    time.sleep(10)
